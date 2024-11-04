a56fa8be2b7b34387ee22eb926efab3f60c8b0c1
mous9270 <mous9270222@gmail.com>
2024-11-04 17:15:25 +0530

    Pricing is added.

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,6 +1,14 @@
+import dynamic from "next/dynamic"
 import CallToAction from "./_components/call-to-action"
 import DashboardSnippet from "./_components/dashboard-snippet"
 
+const PricingSection = dynamic(
+  () =>
+    import("./_components/pricing").then(
+      (component) => component.PricingSection,
+    ),
+  { ssr: true },
+)
 
 export default function Home() {
     return (
@@ -9,6 +17,7 @@ export default function Home() {
           <CallToAction />
           <DashboardSnippet />
         </div>
+        <PricingSection />
         </main>
     )
 }
