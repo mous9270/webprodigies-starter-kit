414a9b1e0d33aa95f574565a52705e24c948089e
mous9270 <mous9270222@gmail.com>
2024-11-05 20:41:40 +0530

    Create sign in page.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,13 +1,15 @@
+import { AuthFormProps, SIGN_IN_FORM, SIGN_UP_FORM } from "./forms"
+
 import {
-    // GROUP_PAGE_MENU,
-    // GroupMenuProps,
-    LANDING_PAGE_MENU,
-    MenuProps,
+  // GROUP_PAGE_MENU,
+  // GroupMenuProps,
+  LANDING_PAGE_MENU,
+  MenuProps,
 } from "./menus"
 type GroupleConstantsProps = {
     landingPageMenu: MenuProps[]
-    // signUpForm: AuthFormProps[]
-    // signInForm: AuthFormProps[]
+    signUpForm: AuthFormProps[]
+    signInForm: AuthFormProps[]
     // groupList: GroupListProps[]
     // createGroupPlaceholder: CreateGroupPlaceholderProps[]
     // groupPageMenu: GroupMenuProps[]
@@ -16,8 +18,8 @@ type GroupleConstantsProps = {
 
 export const GROUPLE_CONSTANTS: GroupleConstantsProps = {
     landingPageMenu: LANDING_PAGE_MENU,
-    // signUpForm: SIGN_UP_FORM,
-    // signInForm: SIGN_IN_FORM,
+    signUpForm: SIGN_UP_FORM,
+    signInForm: SIGN_IN_FORM,
     // groupList: GROUP_LIST,
     // createGroupPlaceholder: CREATE_GROUP_PLACEHOLDER,
     // groupPageMenu: GROUP_PAGE_MENU,
